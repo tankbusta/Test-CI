821d5c51cacfecccf8530b1242c19b265072e9a4
Christopher Schmitt <chris.schmitt@mandiant.com>
2014-04-08 23:46:56 -0400

    yolo

---
+++ tests/test_bacon.py
@@ -0,0 +1,11 @@
+import unittest
+
+
+class BaconStripsNBaconStrips(unittest.TestCase):
+    def test_do_something_ramirez(self):
+        jump = "HOW HIGH?"
+        self.assertEqual(jump, "DO SOMETHING RAMIREZ")
+
+    def test_hashtagyolo(self):
+        var = "#yolo"
+        self.assertEqual(var, "#yolo")
